08c9b05011dc773dfa50c0da5aa1c472f96ae895
mohitgargg <125942814+mohitgargg@users.noreply.github.com>
2023-04-11 12:58:04 +0530

    Add files via upload

---
+++ EXP 2b.c
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+#include <stdio.h>
+
+int main()
+{
+    char name[20];
+    
+{
+    printf("\n Enter SYMBIOSIS: ");
+    scanf("%s",name);
+    printf("%c", name[3]);
+}    
+}
